b9dcf57ffaeb880a20e6f8f96ed6694783d81159
Rohit Bhardwaj <rohitbhardwajindia@gmail.com>
2022-07-06 01:34:12 +0530

    comments & README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+After reading the article "How to become hacker?"
+by Eric S Raymond, I decided to follow the ideas
+mentioned in the article.
+Eric Suggests to read and understand well written code.
+Code should be of programe in which you are interested.
+After reading and understanding code try to add some feature.
+I did the same. After reading code, I added pause feature 
+which was missing in this game.
--- game.py
+++ game.py
@@ -2,15 +2,28 @@ import pygame
 import time
 import random
 
-pygame.init()
+pygame.init() # ??
 
+# color of each sanke block
 black = (0, 0, 0)
+
+# font for game over msg
 red = (255, 0, 0)
+
+
 white = (255, 255, 255)
+
+# background color
 blue = (0, 0, 255)
-yellow = (255, 255, 102)
+
+# font color for score
+yellow = (255, 255, 102) 
+
+# color of food
 green = (0, 255, 0)
 
+
+# dimensions of display
 dis_width = 600
 dis_height = 400
 
@@ -18,37 +31,38 @@ dis_height = 400
 # graphical element on display
 dis=pygame.display.set_mode((dis_width,dis_height)) 
 
-pygame.display.update() #updates changes made to the screen
+
+#updates changes made to the screen
+pygame.display.update() 
+
 # adds caption at the top of game window
 pygame.display.set_caption('Snake game by Rohit')
 
+# for slowing loop
+clock = pygame.time.Clock() 
+
+# height and width of each block of snake
+# also height and width of food 
+snake_block = 10 
+# speed of loop
+snake_speed = 15 
+
+# using built in function for font for showing game over
+font_style = pygame.font.SysFont("bahnschrift", 25) 
+# font for score
+score_font = pygame.font.SysFont("comicsansms", 35) 
 
 
-
-
-
-
-
-
-
-
-
-clock = pygame.time.Clock() # for slowing loop
-
-snake_block = 10 # height and width of each block of snake
-snake_speed = 15 # speed of loop
-
-font_style = pygame.font.SysFont("bahnschrift", 25) # using built in function for font for showing game over
-score_font = pygame.font.SysFont("comicsansms", 35) # font for score
-
+# function for updating score
 def Your_score(score):
     value = score_font.render("Your score: " + str(score), True, yellow)
     dis.blit(value, [0,0])
 
 
 # draws snake 
-def our_snake(snake_block, snake_list):
-    for x in snake_list:
+def our_snake(snake_block, snake_List):
+    # snake_List is list of co-ordinates of each block of snake
+    for x in snake_List:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
 
 
@@ -58,25 +72,35 @@ def message(msg, color):
     dis.blit(mesg, [dis_width / 6, dis_height / 3])
 
 
-
+# function for starting game
 def gameLoop():
+    # closes game window when value is true
     game_over = False
+    # turns true after hitting wall or itself
     game_close = False
+    # turns true after pressing spacebar 
     game_pause = False
 
+    # initial co-ordinate of head of snake
     x1 = dis_width/2
     y1 = dis_height/2    
 
+    # change in co-ordinate of head of snake in each iteration
     x1_change = 0
     y1_change = 0
 
+    # list of each block of snake
     snake_List = []
+    # initial length of snake
     Length_of_snake = 1
 
+    # initial co-ordinate of food
+    # co-ordinate of snake is always multiple of 10
+    # therefore divide and multiply by 10
     foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10
     foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10
 
-
+    # this loop runs the game 
     while not game_over:
 
         # snake hits wall
@@ -92,6 +116,7 @@ def gameLoop():
                         game_over = True
                         game_close = False
                     if event.key == pygame.K_c:
+                        # recursion
                         gameLoop()
 
 
@@ -104,6 +129,8 @@ def gameLoop():
                 game_over = true
 
             # key press to navigate snake
+            # x1_change value can be altered 
+            # even when game is paused
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                         x1_change = -snake_block
@@ -120,7 +147,7 @@ def gameLoop():
                 elif event.key == pygame.K_SPACE:
                         game_pause = not(game_pause)
 
-        # condition for game over
+        # condition for hitting wall
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_close = True
 
@@ -136,6 +163,7 @@ def gameLoop():
             if len(snake_List) > Length_of_snake:
                 del snake_List[0] # delete past posn of tail
 
+            # checks whether snake bite itself
             for x in snake_List[:-1]:
                 if x == snake_Head:
                     game_close = True
@@ -165,4 +193,5 @@ def gameLoop():
     pygame.quit()
     quit()
 
+# starts game
 gameLoop()
